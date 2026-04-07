21957bbf441452cebe20ad9e06b2af7ab8529e20
mdsiyam01325251695016080-maker <mdsiyam01325251695016080@gmail.com>
2026-04-07 11:02:14 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -6,7 +6,7 @@ let xfont = null;
 let yfont = null;
 let categoryEmoji = null;
 
-const HELP_GIF = "https://files.catbox.moe/l0zxb4.mp4";
+const HELP_GIF = "https://files.catbox.moe/g5vr8h.mp4";
 
 async function loadResources() {
   try {
