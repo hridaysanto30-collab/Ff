184d34226ace45d418267a93c161ecd745763ce7
hridaysanto30-collab <hridaysanto30@gmail.com>
2026-04-12 15:38:57 +0800

    Update txt_voice.js

--- scripts/cmds/txt_voice.js
+++ scripts/cmds/txt_voice.js
@@ -32,7 +32,7 @@ module.exports = {
       "সিয়াম ভাই": "https://files.catbox.moe/9w6moo.mp3",
       "সিয়াম": "https://files.catbox.moe/9w6moo.mp3",
 
-      "@তো্ঁমা্ঁগো্ঁ পি্ঁচ্ছি্ঁ উ্ঁদয়্ঁ তা্ঁহ":"https://files.catbox.moe/khdlld.mp4",
+      "@মালয়েশিয়া সিঙ্গেল  বয়":"https://i.imgur.com/r4bmdsa.mp4",
       "@everyone": "htthttps://files.catbox.moe/khdlld.mp4ps://files.catbox.moe/3u6shs.mp3",
       "নিঝুম": "https://files.catbox.moe/5myzdz.mp4",
 
