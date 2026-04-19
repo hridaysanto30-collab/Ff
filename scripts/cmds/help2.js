8a414eddca9c491d0790525e67e87dcf788292fa
hridaysanto30-collab <hridaysanto30@gmail.com>
2026-04-19 16:17:02 +0800

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -8,7 +8,7 @@ module.exports = {
   config: {
     name: "help2",
     version: "1.18",
-    author: "UDAY HASAN SIYAM",
+    author: "মালয়েশিয়া সিঙ্গেল  বয়",
     countDown: 5,
     role: 0,
     shortDescription: { en: "View command usage and list all commands directly" },
@@ -24,9 +24,9 @@ module.exports = {
 
     // New media links
     const mediaLinks = [
-      "https://files.catbox.moe/fiy6vj.gif",
-      "https://files.catbox.moe/a13udh.jpg",
-      "https://files.catbox.moe/41hfau.jpg"
+      "https://i.imgur.com/suwCtfp.jpeg",
+      "https://i.imgur.com/suwCtfp.jpeg",
+      "https://i.imgur.com/suwCtfp.jpeg"
     ];
 
     if (!args[0]) {
@@ -40,7 +40,7 @@ module.exports = {
         categories[category].push(name);
       }
 
-      let msg = `╭───────❁\n│✨ 𝗨𝗗𝗔𝗬 𝗛𝗔𝗦𝗔𝗡 𝗦𝗜𝗬𝗔𝗠 𝗛𝗘𝗟𝗣 𝗟𝗜𝗦𝗧 ✨\n╰────────────❁\n`;
+      let msg = `╭───────❁\n│✨ মালয়েশিয়া সিঙ্গেল  বয় ✨\n╰────────────❁\n`;
 
       for (const cat of Object.keys(categories).sort()) {
         msg += `╭─────✰『  🗂️ ${cat} 』\n`;
@@ -52,7 +52,7 @@ module.exports = {
 
       const totalCommands = Object.values(categories).reduce((a, b) => a + b.length, 0);
       msg += `╭─────✰[🌟 𝐄𝐍𝐉𝐎𝐘 🌟]\n│> TOTAL COMMANDS: [${totalCommands}]\n│\n│> TYPE: [ ${prefix}HELP <COMMAND> ]\n│\n│> FB.LINK: [https://www.facebook.com/MR.FARHAN.420]\n╰────────────✰\n`;
-      msg += `\n╭─────✰\n│ 💖 𝗨𝗗𝗔𝗬 𝗛𝗔𝗦𝗔𝗡 𝗦𝗜𝗬𝗔𝗠 💖\n╰────────────✰`; 
+      msg += `\n╭─────✰\n│ 💖মালয়েশিয়া সিঙ্গেল  বয়💖\n╰────────────✰`; 
 
       const mediaLink = mediaLinks[Math.floor(Math.random() * mediaLinks.length)];
       const stream = await axios.get(mediaLink, { responseType: "stream" }).then(res => res.data);
@@ -71,7 +71,7 @@ module.exports = {
 
       const configCommand = command.config;
       const roleText = roleTextToString(configCommand.role);
-      const author = "UDAY HASAN SIYAM";
+      const author = "মালয়েশিয়া সিঙ্গেল  বয়";
       const longDescription = configCommand.longDescription?.en || "No description";
       const guideBody = configCommand.guide?.en || "No guide available.";
       const usage = guideBody.replace(/{p}/g, prefix).replace(/{n}/g, configCommand.name);
